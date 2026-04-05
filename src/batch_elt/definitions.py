e56349a0cda183f8a4ccfa13bc33253fa9334958
frank <frankcampos85@gmail.com>
2026-04-05 13:51:11 -0500

    feat: add dbt instegration with Dagster and update .gitignore

--- src/batch_elt/definitions.py
+++ src/batch_elt/definitions.py
@@ -1,10 +1,25 @@
-from dagster import Definitions
+from dagster import Definitions, load_from_defs_folder
 from dagster_duckdb import DuckDBResource
+# add dagster cli
+from dagster_dbt import DbtCliResource
+# import  dbt_project 
+from batch_elt.project import dbt_project
 from batch_elt.defs.assets.get_batch_elt_lookup_assets import batch_elt_assets
+from batch_elt.defs.assets.dbt import dbt_all
+from pathlib import Path 
+
+
+
+# dbt cli access to the project
+dbt_resource = DbtCliResource(
+    project_dir=dbt_project,
+)
+# dbt_models = load_from_defs_folder(project_root=Path(__file__)/'assets')
 
 defs = Definitions(
-    assets=batch_elt_assets,
+    assets=batch_elt_assets + [dbt_all],
     resources={
         "database": DuckDBResource(database="duckdb/data.duckdb"),
+        "dbt": dbt_resource,
     },
 )
---
+++ src/batch_elt/defs/assets/dbt.py
@@ -0,0 +1,12 @@
+import dagster as dg 
+from dagster_dbt import DbtCliResource, dbt_assets
+
+from ...project import dbt_project
+
+@dbt_assets(
+    manifest=dbt_project.manifest_path,
+)
+
+def dbt_all(context:dg.AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"],context=context).stream()
+                       
